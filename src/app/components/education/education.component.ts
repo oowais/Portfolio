8def4eecc685e5ce132219a053cbd5025d7401e2
Owais <oowais@gmail.com>
2021-10-20 00:01:09 +0200

    education component updated

--- src/app/components/education/education.component.ts
+++ src/app/components/education/education.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {DataService} from '../../data.service';
 
 @Component({
   selector: 'app-education',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EducationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
   }
@@ -24,4 +25,8 @@ export class EducationComponent implements OnInit {
     'Communications in Digital Age',
     'Language, Mind and Media'
   ]
+
+  openLink (url: string) {
+    this.dataService.openLink(url);
+  }
 }

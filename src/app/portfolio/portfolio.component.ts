70ced46cc21f853c3269f9ddb712c7988ace0ee7
Owais <oowais@gmail.com>
2021-10-20 16:24:57 +0200

    added basic transitions on component level

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,9 +1,20 @@
 import {Component, OnInit} from '@angular/core';
+import {animate, query, stagger, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.scss']
+  styleUrls: ['./portfolio.component.scss'],
+  animations: [
+    trigger('componentsAnim', [
+      transition('* => *', [
+        query('.item', style({opacity: 0, transform: 'translateX(-30px)'})),
+        query('.item', stagger('500ms', [
+          animate('800ms 1.2s ease-out', style({opacity: 1, transform: 'translateX(0)'}))
+        ]))
+      ])
+    ])
+  ]
 })
 export class PortfolioComponent implements OnInit {
   constructor() {

6060a311a4012b8ec5a93ae06858972651bf9adf
Owais <oowais@gmail.com>
2021-10-20 15:39:24 +0200

    about me and header changes

--- src/app/components/about-me/about-me.component.ts
+++ src/app/components/about-me/about-me.component.ts
@@ -6,13 +6,10 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./about-me.component.scss']
 })
 export class AboutMeComponent implements OnInit {
-  aboutMeText = 'Enthusiastic software developer eager to contribute to team success through hard' +
-    'work, versatility, attention to detail and excellent organizational skills. Clear ' +
-    'understanding of Angular and Python and training in Java. Motivated to learn, grow and ' +
-    'excel in software development industry.';
-
-  constructor() {
-  }
+  aboutMeText = 'Hi! my name is Owais Ahmed. I\'m an enthusiastic software developer eager to contribute to team\'s' +
+    ' success through hard work, versatility, attention to detail and excellent organizational skills. Clear' +
+    ' understanding of Angular and Python and training in Java. Motivated to learn, grow and excel in software' +
+    ' development industry.';
 
   ngOnInit(): void {
   }
